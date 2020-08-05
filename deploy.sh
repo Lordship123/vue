eb75ef32a9292c4eaafca17c6dad1fe777b9f4bf
Lordship123 <egor87768@gmail.com>
2020-08-05 20:05:22 +0300

    first commit

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>lordship123.github.io/vuetrainee<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>lordship123.github.io/vuetrainee<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:<lordship123.github.io/>/<vue.github.io/>.git master:gh-pages
 
 cd -
